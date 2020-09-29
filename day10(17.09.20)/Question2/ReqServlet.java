d173fbe7cfa81af33238fb37b39a808e3af52f27
sowmiyamanickam <63553092+sowmiyamanickam@users.noreply.github.com>
2020-09-29 18:42:08 +0530

    Add files via upload

---
+++ day10(17.09.20)/Question2/ReqServlet.java
@@ -0,0 +1,31 @@
+package com.pack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/ReqServlet")
+public class ReqServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReqServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		String name=request.getParameter("name");
+        pw.println("Hello "+ name+"<br/>");
+        pw.close();
+	}
+
+}
